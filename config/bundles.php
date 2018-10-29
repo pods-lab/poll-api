e92d1dd3f9f83bee6f47a4e039b642f882b81d55
Alejandro Quiroz Serna <jakop.box@gmail.com>
2018-10-28 21:28:30 -0500

    Changes in pdf generator.

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
--- src/Controller/GeneratePDFController.php
+++ src/Controller/GeneratePDFController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -11,17 +12,16 @@ use Symfony\Component\HttpFoundation\Request;
  * @package App\Controller
  * @Rest\Route("/v1/pdf")
  */
-class GeneratePDFController
+class GeneratePDFController extends FOSRestController
 {
     /**
      * Generación de PDF
-     * @Rest\Post("/generar", name="generar")
+     * @Rest\Post("/generate", name="generate_pdf_file")
      */
     public function generatePDF(Request $request)
     {
         try{
             $data = json_decode($request->getContent(), true);
-
             global $kernel;
             $twig = $kernel->getContainer()->get("twig");
             $content = $twig->render('generate_pdf/index.html.twig', [
@@ -29,17 +29,32 @@ class GeneratePDFController
             ]);
 
             $mpdf = new \Mpdf\Mpdf();
-
             // Write some HTML code:
             $mpdf->WriteHTML($content);
-
             // Saves file on the server as 'filename.pdf'
-            $pdf = $mpdf->Output('survey.pdf', \Mpdf\Output\Destination::DOWNLOAD);
-
+            $baseDir = realpath($this->get("kernel")->getRootDir() . '/../public/temp');
+            $fileName = time() . ".pdf";
+            header("Content-type:application/pdf");
+            header("Content-Disposition:attachment;filename='$fileName'");
+            header("Access-Control-Allow-Origin: *");
+            $pdf = $mpdf->Output("{$baseDir}/{$fileName}", \Mpdf\Output\Destination::DOWNLOAD);
+            return $pdf;
         } catch (\Exception $exception){
             //ToDo handle error
+            return [
+                'error' => true,
+                'message' => $exception->getMessage(),
+            ];
         }
 
-        return $pdf;
+        /**
+        header("Content-type:application/pdf");
+        header("Content-Disposition:attachment;filename='survey.pdf'");
+        header("Access-Control-Allow-Origin: *");
+         */
+        return [
+            'status' => true,
+            'data' => $pdf
+        ];
     }
 }
